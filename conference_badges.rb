96bc784d8e786cc8ede5ad0b93431512094f593e
James Lenahan <james.k.lenahan@gmail.com>
2019-11-29 15:42:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,35 +1,31 @@
-# Write your code here.
-people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name)
-  nuarray = []
-  Array.each do |name|
-    nuarray.push("Hello, my name is #{name}.")
+def batch_badge_creator(arr)
+  new_array = []
+  arr.each do |name|
+    new_array.push("Hello, my name is #{name}.")
   end
-  return nuarray
+  return new_array
 end
 
-def assign_rooms(array)
-  nuarray = []
+def assign_rooms(arr)
+  new_array = []
   counter = 1
-  array.each do |name|
-    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter += 1
+  arr.each do |name|
+  new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+  counter+=1
   end
-  return nuarray
+  return new_array  
 end
 
-# assign_rooms(people)
+def printer(arr)
+  batch_badge_creator(arr).each do |badge|
+    puts badge
+  end
 
-def printer(array)
-  batch_badge_creator(array).each do |id|
-    puts id
-  end 
-  
-  assign_rooms(array).each do |id|
-    puts id 
+  assign_rooms(arr).each do |assignment|
+    puts assignment
   end
 end 
